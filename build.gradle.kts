d5e5065e11ecad89723dd287c61299e753d9140f
Choi HyunJoon <builder_joon@kakao.com>
2024-08-14 14:50:28 +0900

    release 브랜치 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.ua-parser:uap-java:1.6.1")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
--- src/main/java/org/builder/joon/entity/VisitorInfoEntity.java
+++ src/main/java/org/builder/joon/entity/VisitorInfoEntity.java
@@ -1,14 +1,12 @@
 package org.builder.joon.entity;
 
+import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ua_parser.Client;
-import ua_parser.Parser;
-
 import java.time.LocalDateTime;
 
 @Data
@@ -26,9 +24,7 @@ public class VisitorInfoEntity {
     private String ipAddress;
     private String requestUrl;
     private String referer;
-    private String browser;
-    private String os;
-    private String device;
+    private String userAgent;
     private LocalDateTime createdAt;
 
     @PrePersist
@@ -37,17 +33,12 @@ public class VisitorInfoEntity {
     }
 
     public static VisitorInfoEntity from(HttpServletRequest request) {
-        String userAgent = request.getHeader("User-Agent");
-        Parser parser = new Parser();
-        Client client = parser.parse(userAgent);
-
         return VisitorInfoEntity.builder()
-                .ipAddress(request.getRemoteAddr())
+                .ipAddress(StringUtils.isNotBlank(request.getHeader("X-Forwarded-For")) ?
+                        "user: "+ request.getHeader("X-Forwarded-For") : "loadbalancer: " + request.getRemoteAddr())
                 .requestUrl(request.getRequestURL().toString())
                 .referer(request.getHeader("Referer"))
-                .browser(client.userAgent.family + " " + client.userAgent.major + "." + client.userAgent.minor)
-                .os(client.os.family + " " + client.os.major + "." + client.os.minor)
-                .device(client.device.family)
+                .userAgent(request.getHeader("User-Agent"))
                 .build();
     }
 }
