fc2bb39a4a05743a3f86ec55d15cd2ce1d34abc7
Choi HyunJoon <builder_joon@kakao.com>
2024-08-16 00:07:23 +0900

    GitHub Actions & AWS CodeDeploy 로 CI/CD 설정 v1.5
    - jar 파일 그대로 s3에 업로드하도록 gradle.yml 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-REPOSITORY=/home/ubuntu/action
-cd $REPOSITORY
+APPLICATION_DIR=/home/ubuntu/application
+cd $APPLICATION_DIR
 
 # JAR 파일 경로 찾기
-JAR_NAME=$(find $REPOSITORY -name '*.jar' | tail -n 1)
+JAR_NAME=$(find $APPLICATION_DIR -name '*.jar' | tail -n 1)
 JAR_PATH=$JAR_NAME
 
 # 현재 실행 중인 애플리케이션 프로세스 ID 찾기
@@ -13,8 +13,8 @@ CURRENT_PID=$(pgrep -fl java | grep $JAR_NAME | awk '{print $1}')
 if [ -z "$CURRENT_PID" ]; then
     echo "애플리케이션이 실행 중이지 않습니다."
 else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
+    echo "> kill -9 $CURRENT_PID"
+    kill -9 $CURRENT_PID
     sleep 5
 fi
 
@@ -22,4 +22,4 @@ echo "> $JAR_PATH 에 실행 권한 추가"
 chmod +x $JAR_PATH
 
 echo "> $JAR_PATH 배포"
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_PATH &
+java -jar -Duser.timezone=Asia/Seoul $JAR_PATH &
