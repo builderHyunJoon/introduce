96d56429aab2995a4363f3cb6d37ef36f04c0eb9
Choi HyunJoon <builder_joon@kakao.com>
2024-09-04 20:23:48 +0900

    PortfolioInfoRepository의 식별자 타입 올바르게 지정

--- src/main/java/org/builder/joon/repository/PortfolioInfoRepository.java
+++ src/main/java/org/builder/joon/repository/PortfolioInfoRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 
-public interface PortfolioInfoRepository extends JpaRepository<PortfolioInfoEntity, Long> {
+public interface PortfolioInfoRepository extends JpaRepository<PortfolioInfoEntity, String> {
     List<PortfolioInfoEntity> findAllByOrderByPortfolioIdxAsc();
 }
